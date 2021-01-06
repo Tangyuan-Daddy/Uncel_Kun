3a94d82ca45c61015de43456f2c4a04deb397998
minkun <minkun@ihomefnt.com>
2021-01-06 14:41:55 +0800

    计算个税

---
+++ src/main/java/org/tydd/common/CalculationTaxCommon.java
@@ -0,0 +1,20 @@
+package org.tydd.common;
+
+/**
+ * @author minkun
+ * @Project UncleK
+ * @Package org.tydd.common
+ * @Description 个税计算常量
+ * @Date 2021/1/6
+ */
+public class CalculationTaxCommon {
+
+    /** 选中 */
+    public final static String CHECK_FLAG = "1";
+
+    /** 未选中 */
+    public final static String NOT_CHECK_FLAG = "2";
+
+    /** 赡养老人 - 均分 */
+    public final static String SPORT_OLD_MAN_AVERAGE_FLAG = "3";
+}
--- src/main/java/org/tydd/webmagic/lianjia/controller/WebMagicController.java
+++ src/main/java/org/tydd/webmagic/lianjia/controller/WebMagicController.java
@@ -1,4 +1,4 @@
-package org.tydd.controller;
+package org.tydd.webmagic.lianjia.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
